9d19acbbf2b87ea69d55419301b3940257ef58d0
ehrsystem <sampat.dasari@gmail.com>
2014-12-28 20:56:40 +0530

    EHRApplication
    base copy

---
+++ EHRSampleApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EHRSampleApplication.Startup))]
+namespace EHRSampleApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
